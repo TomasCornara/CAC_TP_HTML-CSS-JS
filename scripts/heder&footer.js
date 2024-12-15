a9069e93735a14cc1c6a84c585f70c9e582607a3
Tomas Cornara <55905365+TomasCornara@users.noreply.github.com>
2024-12-15 19:45:07 -0300

    Update heder&footer.js

--- scripts/heder&footer.js
+++ scripts/heder&footer.js
@@ -25,7 +25,7 @@ createApp({
     components: {
 
         "sec_footer":{
-            template: '<footer><section id="redes"> <a href="https://twitter.com/" target="_blank"> <i class="fa fa-twitter-square fa"></i> </a> <a href="https://www.facebook.com/" target="_blank"> <i class="fab fa-facebook-f"> </i> </a> <a href="https://www.pinterest.com/" target="_blank"> <i class="fab fa-instagram"></i> </a> <a href="https://www.linkedin.com/" target="_blank"> <i class="fab fa-linkedin-in"></i> </a></section> <section id="subFooter"> <a href="https://github.com/JonatanMartinez1310" target="_blank">Jonatan Martínez</a> <a href="https://github.com/Pettu93" target="_blank">Cintia Cuevas</a> <a href="https://github.com/PabloDanielMoyano" target="_blank">Pablo Moyano</a> <a href="https://github.com/TomasCornara" target="_blank">Tomas Cornara</a> </section></footer>'
+            template: '<footer><section id="redes"> <a href="https://twitter.com/" target="_blank"> <i class="fa fa-twitter-square fa"></i> </a> <a href="https://www.facebook.com/" target="_blank"> <i class="fab fa-facebook-f"> </i> </a> <a href="https://www.pinterest.com/" target="_blank"> <i class="fab fa-instagram"></i> </a> <a href="https://www.linkedin.com/" target="_blank"> <i class="fab fa-linkedin-in"></i> </a></section> <section id="subFooter"> <a href="https://github.com/JonatanMartinez1310" target="_blank">Jonatan Martínez</a> <a href="https://github.com/Pettu93" target="_blank">Cintia Cuevas</a> <a href="https://github.com/TomasCornara" target="_blank">Tomas Cornara</a> </section></footer>'
         },
 
     }
